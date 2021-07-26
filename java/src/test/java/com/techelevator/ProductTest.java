400432a9b88662bc904c1493580874049f5a44e6
mariopaz[D[D[D[C[C[C[D[D[D[D[C [D[D[D[D[M[C[C[C[C[C[C[DP[C[C[az <paz.mario.j@gmail.com>
2021-07-25 18:33:42 -0600

    more tests

--- java/src/test/java/com/techelevator/ProductTest.java
+++ java/src/test/java/com/techelevator/ProductTest.java
@@ -3,7 +3,7 @@ package com.techelevator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
-
+import java.lang.String;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
@@ -28,18 +28,55 @@ public class ProductTest {
         Product product = new Product("A1", "Potato Crisps", new BigDecimal(3.05), 5);
 
 
+        String actual = product.getName();
+        String expected = "Potato Crisps";
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void getPrice_enter_get_3dec05() {
+
+        Product product = new Product("A1", "Potato Crisps", new BigDecimal(3.05), 5);
+
+
+        BigDecimal actual = product.getPrice();
+        BigDecimal expected = new BigDecimal(3.05);
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void getQuantity_enter_get_5() {
+
+        Product product = new Product("A1", "Potato Crisps", new BigDecimal(3.05), 5);
+
+
+        int actual = product.getQuantity();
+        int expected = 5;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void set_Sound_Test() {
+
+
+        Chip chip = new Chip("A1", "Potato Crisp", new BigDecimal(3.05), 5);
+
+        String actual = chip.getSound();
+        String expected = "Crunch, crunch yum!";
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void set_Slot_Test() {
+
+
+        Product product = new Product("A1", "Potato Crisps", new BigDecimal(3.05), 5);
+
         String actual = product.getSlot();
         String expected = "A1";
 
         Assert.assertEquals(expected, actual);
-
     }
-
 }
-
-
-//    @Before
-//    public void setup() {
-//
-//    }
-
